ed955cf17f3200aa59ca88ef784709b1bd6f30ef
bongjae0523 <98721987+bongjae0523@users.noreply.github.com>
2022-11-11 17:06:15 +0900

    Update WildCardUnboxerEx01.java

--- Exam_20221110/src/com/ezen/ex9/WildCardUnboxerEx01.java
+++ Exam_20221110/src/com/ezen/ex9/WildCardUnboxerEx01.java
@@ -21,18 +21,20 @@ class Toy{
 }
 
 class Boxhander{
+//				get 가능, set 불가능
 	public static void outBox(Box<? extends Toy> box) {
 		Toy t = box.get();
 //		box.set(new Toy()); 
-//		에러. Toy 이거나 Toy를 상속한 대상만 가능. 위 Box의 set은 상위 클래스인 Objict이므로 안됨.
+//		에러. Toy 이거나 Toy를 상속한 대상만 가능. 
 		System.out.println(t);
 	}
-	
-	public static void inBox(Box<Toy> box, Toy n) {
+//				set 가능, get 불가능
+	public static void inBox(Box<? super Toy> box, Toy n) {
 		box.set(n);
+//		box.get(n);
+//		에러. Toy 이거나 Object 가능. 
 	}
 }
-
 
 public class WildCardUnboxerEx01 {
 
@@ -43,5 +45,10 @@ public class WildCardUnboxerEx01 {
 		Boxhander.outBox(box);
 		
 	}
-
+	
+	public static void moveBox(Box < ? super Toy> a, Box< ? extends Toy> b){
+//				Toy,Object				Toy,Toy 상속받은
+//				set만 가능				get만 가능
+//				b가 a로 이동 그냥 외워
+	}
 }
